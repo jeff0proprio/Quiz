27b73b4249a1d96ba1c7af8d2e5f96a3e1a9af75
JEFFERSON LUIZ VIEIRA <jefferson_l_vieira@estudante.sesisenai.org.br>
2024-07-10 12:00:52 -0300

    Quiz

--- Quiz (HTML + CSS + JavaScript )/file.js
+++ Quiz (HTML + CSS + JavaScript )/file.js
@@ -6,20 +6,20 @@ function FinalizarQuiz(){
     let Pontuacao = document.getElementById("PontuacaoTotal")
     
     
-    Pontuacao.value = (Number(RespostaUm.value) + Number(RespotaDois.value) + Number(RespostaTres.value))
     
     if(RespostaUm.value == 2 || RespotaDois.value == 2 || RespostaTres.value == 2){
         
         alert("assinale todas as respostas !")
         
         return;
-
+        
     }else{
-
+        
         alert("Quiz Finalizado! Confira sua pontuação")
-
+        
     }
-
+    
+    Pontuacao.value = (Number(RespostaUm.value) + Number(RespotaDois.value) + Number(RespostaTres.value))
 
     if(Pontuacao.value == 3){
 
